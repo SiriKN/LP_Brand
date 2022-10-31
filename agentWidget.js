fe89045d7394ded4ad9742a5e244b73d7bb92987
SiriKN <skollipara@liveperson.com>
2022-10-31 14:03:11 +0000

    v2

---
+++ agentWidget.js
@@ -0,0 +1,40 @@
+lpTag.agentSDK.init()
+var updateCallback = function(data) {
+    // get convo lines as they happen
+    var newLine = data.newValue;
+    let usertext = document.getElementById("usertext");
+    let agenttext = document.getElementById("agenttext");
+    if (newLine[0].by == "Visitor") {
+        usertext.innerHTML = newLine[0].text;
+        console.log('setting visitor text');
+    } else {
+        agenttext.innerHTML = newLine[0].text;
+        console.log('setting agent text');
+    }
+    var ccs = document.getElementById("ccs");
+    var ccsData = ccs.innerHTML;
+    console.log("ccs data element: " + ccsData)
+    if (ccsData == "") {
+        // API call to grab data
+        let sessionId = ""
+        let onSuccessSession = (data) => {
+            sessionId = data; 
+            console.log("sessionId:  " + sessionId);
+            
+        }
+        let onFailure = (err) => {
+            console.log(err);
+        }
+        var sessionPath = "chatInfo.rtSessionId";
+        lpTag.agentSDK.get(sessionPath, onSuccessSession, onFailure);
+    }
+};
+
+var notifyWhenDone = function(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log("set the value");
+};
+var pathToData = "chatTranscript.lines";
+lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
